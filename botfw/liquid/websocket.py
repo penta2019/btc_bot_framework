17e93bfa3b061e2d1e45376dfda9abd56b11ffe0
@_P_E_N_T_A <>
2020-02-14 15:15:23 +0900

    liquid websocket認証

--- botfw/liquid/websocket.py
+++ botfw/liquid/websocket.py
@@ -1,11 +1,8 @@
-# import time
-# import secrets
-# import hmac
-# import hashlib
 import json
 import traceback
 
 from ..base.websocket import WebsocketBase
+from .jwt import create_jwt
 
 
 class LiquidWebsocket(WebsocketBase):
@@ -13,27 +10,33 @@ class LiquidWebsocket(WebsocketBase):
 
     def __init__(self, key=None, secret=None):
         super().__init__(self.ENDPOINT)
-        # self.__key = key
-        # self.__secret = secret
-        self.__request_table = {}  # (msg, cb)
+        self.__key = key
+        self.__secret = secret
         self.__ch_cb_map = {}
 
-        # if self.__key and self.__secret:
-        #     self.add_after_open_callback(
-        #         lambda: self.authenticate(self.__key, self.__secret))
+        if self.__key and self.__secret:
+            self.add_after_open_callback(
+                lambda: self.authenticate(self.__key, self.__secret))
 
-    def command(self, op, args=[], cb=None):
+    def command(self, op, args=[]):
         msg = {'event': op, 'data': args}
         self.send(msg)
-        self.__request_table[str(args)] = (msg, cb)
-        return args
+        self.log.info(f'{msg}')
 
     def subscribe(self, ch, cb):
         self.command('pusher:subscribe', {'channel': ch})
         self.__ch_cb_map[ch] = cb
 
+    def authenticate(self, key, secret):
+        auth_payload = {
+            'path': '/realtime',
+            'headers': {
+                'X-Quoine-Auth': create_jwt(key, secret),
+            },
+        }
+        self.command('quoine:auth_request', auth_payload)
+
     def _on_open(self):
-        self.__request_table = {}
         self.__ch_cb_map = {}
         super()._on_open()
 
@@ -46,15 +49,15 @@ class LiquidWebsocket(WebsocketBase):
             elif e == 'updated':
                 self.__ch_cb_map[msg['channel']](msg)
             elif e == 'pusher_internal:subscription_succeeded':
-                key = str({'channel': msg['channel']})
-                req, cb = self.__request_table[key]
-                res = msg['data']
-                self.log.info(f'{req} => {res}')
-                if cb:
-                    cb(msg)
+                ch = msg['channel']
+                self.log.info(f'subscription succeeded: {ch}')
+            elif e == 'quoine:auth_success':
+                self._set_auth_result(True)
+            elif e == 'quoine:auth_failure':
+                self._set_auth_result(False)
             elif e == 'pusher:connection_established':
                 pass
             else:
-                self.log.warning(f'Unknown message {msg}')
+                self.log.warning(f'Unknown message: {msg}')
         except Exception:
             self.log.error(traceback.format_exc())
