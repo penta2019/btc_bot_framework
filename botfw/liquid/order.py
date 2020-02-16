98e6453ef4f05aa3f8ceaf2a219304a6baba5503
@_P_E_N_T_A <>
2020-02-16 22:50:43 +0900

    liquid order 追加（未完成）

---
+++ botfw/liquid/order.py
@@ -0,0 +1,36 @@
+from ..base import order as od
+from .api import LiquidApi
+from ..etc.util import unix_time_from_ISO8601Z, decimal_add
+
+
+class LiquidOrderManager(od.OrderManagerBase):
+    SYMBOLS = ['BTC/JPY', 'ETH/JPY', 'XRP/JPY']
+
+    def _after_auth(self):
+        for symbol in self.SYMBOLS:
+            s = symbol.replace('/', '').lower()
+            self.ws.subscribe(f'user_executions_cash_{s}', self.__on_events)
+
+    def _generate_order_object(self, e):
+        print('_generate_order_object', e)
+
+    def __on_events(self, msg):
+        print('__on_events', msg)
+        # self._handle_order_event(oe)
+
+
+class LiquidPositionGroup(od.PositionGroupBase):
+    def __init__(self):
+        super().__init__()
+        self.commission = 0  # total commissions
+
+    def update(self, price, size, info):
+        super().update(price, size)
+
+
+class LiquidOrderGroup(od.OrderGroupBase):
+    PositionGroup = LiquidPositionGroup
+
+
+class LiquidOrderGroupManager(od.OrderGroupManagerBase):
+    OrderGroup = LiquidOrderGroup
