e22c6029c57530060ab0bb3f2277dda42419fa8a
@_P_E_N_T_A <>
2020-03-04 07:14:28 +0900

    サンプル不具合修正

--- samples/etc/subscribe.py
+++ samples/etc/subscribe.py
@@ -1,11 +1,8 @@
-# 約定・板以外のチャンネルを購読する方法
-# Websocketクラスは内部でコネクションを一つ生成します。
-# subscribeは内部のコネクションが生成された後に呼び出す必要があるため、
-# 以下のようにadd_after_open_callbackにsubscribeを行うコールバックを追加してください。
-# このコールバックはコネクション切断による再接続の際にも自動で呼び出されます。
+# 約定・板以外のチャンネルの購読
+# subscribe()の第二引数にチャンネルメッセージを処理するコールバック関数を渡してください。
+# 切断時は自動的に再接続・チャンネル購読を行います。
 
 import botfw
 ws = botfw.Bitflyer.Websocket()
-ws.add_after_open_callback(
-    lambda: ws.subscribe('lightning_ticker_BTC_JPY', print))
+ws.subscribe('lightning_ticker_BTC_JPY', print)
 input()
