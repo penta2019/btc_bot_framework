e058e55462eb261351ccfe3dc302c1b21fb6cc0b
@_P_E_N_T_A <>
2020-04-19 15:57:15 +0900

    bybit fetch_position()追加

--- botfw/bybit/api.py
+++ botfw/bybit/api.py
@@ -9,3 +9,12 @@ class BybitApi(ApiBase, ccxt.bybit):
         ApiBase.__init__(self)
         ccxt.bybit.__init__(self, ccxt_config)
         self.load_markets()
+
+        # silence linter
+        self.private_get_position_list = getattr(
+            self, 'private_get_position_list')
+
+    def fetch_position(self, symbol):
+        market_id = self.market_id(symbol)
+        res = self.private_get_position_list({'symbol': market_id})['result']
+        return -res['size'] if res['side'] == 'Sell' else res['size']
