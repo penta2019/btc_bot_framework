28fcbfa1995ab0a13de03bc0fc2d81b485de18e9
@_P_E_N_T_A <>
2020-06-19 04:11:05 +0900

    trade proxyのwebsocket_serverをwebsocketsに変更

--- samples/etc/trade_proxy.py
+++ samples/etc/trade_proxy.py
@@ -13,79 +13,95 @@
 import logging
 import json
 import sys
+import asyncio
 
-from websocket_server import WebsocketServer
+import websockets
 
 import botfw as fw
 
-server_port, debug_port = sys.argv[1:]
 
-log = logging.getLogger()
-fw.setup_logger()
+class TradeProxy:
+    def __init__(self, port):
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.clients = {}  # {client: ((exchange, symbol), callback)}
+        self.trades = {}  # {(exchange, symbol)}
+        self.server = websockets.serve(self.handle_ws, "localhost", port)
+        self.loop = asyncio.get_event_loop()
 
-# デバッグ用
-cmd = fw.Cmd(globals())
-cmd_server = fw.CmdServer(int(debug_port))
-cmd_server.register_command(cmd.eval)
-cmd_server.register_command(cmd.exec)
-cmd_server.register_command(cmd.print, log=False)
+    def run(self):
+        self.loop.run_until_complete(self.server)
+        self.loop.run_forever()
 
-clients = {}  # {client: ((exchange, symbol), callback)}
-trades = {}  # {(exchange, symbol)}
+    async def handle_ws(self, ws, path):
+        self.on_new_client(ws)
+        try:
+            while True:
+                msg = await ws.recv()
+                self.on_message_received(ws, msg)
+        except websockets.ConnectionClosedOK as e:
+            pass
+        except Exception as e:
+            self.log.error(e)
+        self.on_client_left(ws)
+
+    def on_new_client(self, ws):
+        addr = ws.remote_address
+        self.log.info(f'{addr}: OPEN')
+
+        self.clients[addr] = ()
+
+    def on_client_left(self, ws):
+        addr = ws.remote_address
+        self.log.info(f'{addr}: CLOSE')
+
+        info = self.clients[addr]
+        if info:
+            key, cb = info
+            t = self.trades[key]
+            t.remove_callback(cb)
+            if not t.cb:
+                t.ws.stop()
+                del self.trades[key]
+
+        del self.clients[addr]
+
+    def on_message_received(self, ws, msg):
+        addr = ws.remote_address
+        self.log.info(f'{addr}: "{msg}"')
+
+        data = json.loads(msg)
+        exchange = data['exchange']
+        symbol = data['symbol']
+        key = (exchange, symbol)
+
+        if self.clients[addr]:
+            self.log.error(f'{addr}: already subscribed channel')
+            return
+
+        t = self.trades.get(key)
+        if not t:
+            ex = getattr(fw, exchange)
+            t = ex.Trade(symbol)
+            self.trades[key] = t
+
+        def cb(ts, price, size):
+            asyncio.run_coroutine_threadsafe(
+                ws.send(json.dumps([ts, price, size])), self.loop)
+
+        t.add_callback(cb)
+        self.clients[addr] = (key, cb)
 
 
-def on_new_client(client, server):
-    addr = client['address']
-    log.info(f'{addr}: OPEN')
+if __name__ == '__main__':
+    server_port, debug_port = sys.argv[1:]
+    fw.setup_logger()
 
-    clients[addr] = ()
+    # デバッグ用
+    cmd = fw.Cmd(globals())
+    cmd_server = fw.CmdServer(int(debug_port))
+    cmd_server.register_command(cmd.eval)
+    cmd_server.register_command(cmd.exec)
+    cmd_server.register_command(cmd.print, log=False)
 
-
-def on_client_left(client, server):
-    addr = client['address']
-    log.info(f'{addr}: CLOSE')
-
-    info = clients[addr]
-    if info:
-        key, cb = info
-        t = trades[key]
-        t.remove_callback(cb)
-        if not t.cb:
-            t.ws.stop()
-            del trades[key]
-
-    del clients[addr]
-
-
-def on_message_received(client, server, message):
-    addr = client['address']
-    log.info(f'{addr}: "{message}"')
-
-    data = json.loads(message)
-    exchange = data['exchange']
-    symbol = data['symbol']
-    key = (exchange, symbol)
-
-    if clients[addr]:
-        log.error(f'{addr}: already subscribed channel')
-        return
-
-    t = trades.get(key)
-    if not t:
-        ex = getattr(fw, exchange)
-        t = ex.Trade(symbol)
-        trades[key] = t
-
-    def cb(ts, price, size):
-        server.send_message(client, json.dumps([ts, price, size]))
-
-    t.add_callback(cb)
-    clients[addr] = (key, cb)
-
-
-if __name__ == "__main__":
-    server = WebsocketServer(port=int(server_port), host='127.0.0.1')
-    server.set_fn_new_client(on_new_client)
-    server.set_fn_client_left(on_client_left)
-    server.set_fn_message_received(on_message_received)
-    server.run_forever()
+    proxy = TradeProxy(server_port)
+    proxy.run()
